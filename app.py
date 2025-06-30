616f89c31fb4f7ab9c4d9473865b4318ff0458df
elioruiz <eliecer.ruiz@udea.edu.co>
2025-06-29 22:26:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.set_page_config(page_title="🧹 Visualizador de Limpieza", layout="centered"
 
 # --- CONEXIÓN A MONGO ---
 import os
-MONGO_URI = os.environ["mongo_uri"]
+MONGO_URI = os.environ["MONGO_URI"]
 client = pymongo.MongoClient(MONGO_URI)
 db = client.cleanup
 collection = db.entries
